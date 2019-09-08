fe1006adbc9e808a2a1a340714e5b3e2c0f8f418
André Matthies <matthiez@gmail.com>
2019-09-08 21:20:48 +0200

    NutrinfoActive: removed unused field unit

--- src/Entity/NutrinfoActive.php
+++ src/Entity/NutrinfoActive.php
@@ -30,12 +30,6 @@ class NutrinfoActive implements ResourceInterface, TranslatableInterface
      */
     protected $id;
 
-    /**
-     * @Column(type="string")
-     * @var null|string
-     */
-    protected $unit;
-
     public function __construct()
     {
         $this->initializeTranslationsCollection();
@@ -62,18 +56,6 @@ class NutrinfoActive implements ResourceInterface, TranslatableInterface
         $translation = $this->getTranslation();
 
         return $translation->getName();
-    }
-
-    public function getUnit(): ?string
-    {
-        return $this->unit;
-    }
-
-    public function setUnit(string $unit): self
-    {
-        $this->unit = $unit;
-
-        return $this;
     }
 
     /**
